b93693176730ce43e2fcc99e9a2a4fd0216dbafe
ktiet1 <ktiet1@uci.edu>
2017-01-24 15:40:25 -0800

    json test

--- js/search.js
+++ js/search.js
@@ -1,30 +1,62 @@
-const championgg_token = "13a49d2950a0a74457b1d3379c490850";
-const GG = require('./node-gg');
-const gg = GG.init('13a49d2950a0a74457b1d3379c490850');
-const ddItems = require('./data-dragon/7.1.1/data/en_US/item.json');
-const ddChampions = require('./data-dragon/7.1.1/data/en_US/champion.json');
+const ggKey = '13a49d2950a0a74457b1d3379c490850';
+const hostname = 'api.champion.gg';
+const port = 80;
 
-var returnSkills = "";
-var returnBuild = "";
+function loadJSON(file, callback) {
 
-skillOrder = function(response, json) {
-  var skills = "";
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', file, true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);
+ }
+
+
+function load() {
+
+    loadJSON("./js/data-dragon/7.1.1/data/en_US/item.json", function(response) {
+
+        var actual_JSON = JSON.parse(response);
+        console.log(actual_JSON);
+    });
+
+
+}
+
+function skillOrder(response, json) {
+  var skill = "";
   json[0].order.forEach(function(value) {
     skill += value;
   })
-  returnSkills = skills;
+  return skill;
 }
 
 //gg.champions.items.finished.popular("Nunu", itemBuild);//
-itemBuild = function(response, json) {
+function itemBuild(json) {
   var build = "";
   json[0].items.forEach(function(value) {
-    build += ddItems.data[value].name + "\n";
+    build += itemData[value].name + "\n";
   });
-  returnBuild = build;
+  return build;
 }
 
-champSearch = function() {
-  //return document.getElementById("demo").innerHTML = gg.champions.items.finished.popular(name, itemBuild);
-  document.getElementById("demo").innerHTML = <p> + "Hello" + </p>;
+function clicked() {
+  load();
+
+}
+
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
 }
