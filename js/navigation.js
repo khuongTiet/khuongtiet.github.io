10c5253430ad5283781926497c989400e52aee9c
ktiet1 <ktiet1@uci.edu>
2017-01-26 21:22:53 -0800

    used js to write header on all files

---
+++ js/navigation.js
@@ -0,0 +1,14 @@
+document.getElementById("header-content").innerHTML =
+'<ul class="topnav" id ="headerTopNav">' +
+  '<li><a href="./index.html">Home</a></li>' +
+  '<li class="dropdown">' +
+    '<a href="javascript:void(0)" class="dropbtn">Projects</a>' +
+    '<div class="dropdown-content">' +
+      '<a href=./sgsearch.html>SGSearch</a>' +
+      '<a href=./choose.html>Choose</a>' +
+      '<a href=#WIP2>WIP</a>' +
+    '</div>' +
+  '</li>' +
+  '<li><a href=./pdf/Resume.pdf>Resume</a></li>' +
+  '<li><a href="./contact.html">Contact</a></li>' +
+'</ul>';
