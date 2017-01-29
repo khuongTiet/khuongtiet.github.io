c7477ec74be00787b1759bfdac409326a0764245
ktiet1 <ktiet1@uci.edu>
2017-01-28 21:39:56 -0800

    added github link in navigation bar

--- js/navigation.js
+++ js/navigation.js
@@ -1,14 +1,15 @@
 document.getElementById("header-content").innerHTML =
 '<ul class="topnav" id ="headerTopNav">' +
-  '<li><a href="./index.html">Home</a></li>' +
+  '<li><a href="./index">Home</a></li>' +
   '<li class="dropdown">' +
     '<a href="javascript:void(0)" class="dropbtn">Projects</a>' +
     '<div class="dropdown-content">' +
-      '<a href=./sgsearch.html>SGSearch</a>' +
-      '<a href=./choose.html>Choose</a>' +
+      '<a href=./sgsearch>SGSearch</a>' +
+      '<a href=./choose>Choose</a>' +
       '<a href=#WIP2>WIP</a>' +
     '</div>' +
   '</li>' +
   '<li><a href=./pdf/Resume.pdf>Resume</a></li>' +
-  '<li><a href="./contact.html">Contact</a></li>' +
+  '<li><a href="./contact">Contact</a></li>' +
+  '<li style="float:right"><a href="khuongtiet.github.io"><img src=./img/giticon3.png></a></li>' +
 '</ul>';
