37d857a8194abd0a26a4e5398a1a6ee9545acd0c
khuongTiet <khuongtiet@gmail.com>
2017-11-13 23:56:10 -0800

    INF133 added more sections

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
-$(function() {
-  $('.info').on('click', '.info-head', function(e) {
-    $(e.target).next().toggle();
+$(function () {
+  $('.info').on('click', '.info-head', function (e) {
+    $(e.target).next().fadeToggle();
   });
+
+  $('.courses').on('click', '.course-name', function (e) {
+    $(e.target).next().slideToggle();
+  })
 });
